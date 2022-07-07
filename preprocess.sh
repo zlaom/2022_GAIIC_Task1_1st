cd5a3222b0f595a32c5e39cbea623f5042b42a4c
laoming zhang <lpancake@foxmail.com>
2022-07-07 15:27:56 +0800

    final code v1.0

--- preprocess.sh
+++
@@ -1,6 +0,0 @@
-python project/code/dataset/data_process/data_preprocess.py
-
-python project/code/dataset/data_process/split_words.py --gpus '3'
-
-python project/code/dataset/data_process/process_split_words.py
-
